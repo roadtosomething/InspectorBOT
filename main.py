e8fb69cbb7b75d121e51cbebf1213e4941105f86
kyalov <hikker1998@gmail.com>
2022-07-27 10:53:41 +0300

    Чтобы сервер не вылетал

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import telebot
-
+from time import sleep
 
 bot = telebot.TeleBot('5530656760:AAGAgKuFsVqZlneTqcPmy-z2u3VtlUlHHP4')
 lang = 'ru'
 
-
-
-@bot.message_handler(content_types=['text'])
+@bot.message_handler()
 def get_text_message(message):
     str = " "
     targetText = message.text.lower()
+    print(targetText)
     trigerMikova = ["@diamikova","затян","обработай","проставь","смени","PKO_0000_0208",'свяжи','связа','формир','синхронизируй']
     trigerTrukhacheva = ["@Elenka_Evgen","смени","проставь","PKO_0000_0208"]
     trigerPokholkov = ["@Krasnoff_YT","обработай","затян",'свяжи','синхронизируй']
     trigerLamskov = ["@lamskoff","затян","обработай",'свяжи','связа','формир']
-    trigerGudkov = ["@Georgiy_Gudkov","ошибка","смени","проставь",'поправь','ПГ_ИК_4004','посмотри']
-    trigerGilev = ["@Smiiiita","ЭА2020_ИК_10127","спек","спецификаци",'замени']
-    trigerVoronin = ["@jlmdie","ЭА2020_ИК_10127","спек","спецификаци",'sid','сид']
-    trigerLuchnikov = ['@Melogor','Сумма контракта превышает предложение участника']
-    trigerMaksimov  = ['@MaksimovVM','Сумма контракта превышает предложение участника']
+    trigerGudkov = ["@Georgiy_Gudkov","ошибка","смени","проставь",'поправь','пг_ик_4004','посмотри','убери','смп']
+    trigerGilev = ["@Smiiiita","эа2020_ик_10127","спек","спецификаци",'замени']
+    trigerVoronin = ["@jlmdie","эа2020_ик_10127","спек","спецификаци",'sid','сид']
+    trigerLuchnikov = ['@Melogor','Сумма контракта превышает предложение участника','apk_0000_0614']
+    trigerMaksimov  = ['@MaksimovVM','Сумма контракта превышает предложение участника','apk_0000_0614']
     trigerSedov = ["@mis_sed",'финансирован']
 
     def tagName(triger):
@@ -27,12 +26,12 @@ def get_text_message(message):
                 str = triger[0]+" "
                 break
             else:
-                str=''
+                str = ''
         return str
 
 
     if "/contract/" in targetText:
-        if ((" сид" not in targetText) and (' sid ' not in targetText)):
+        if 'sid' not in targetText or 'сид' not in targetText:
             str += tagName(trigerMikova)
             str += tagName(trigerTrukhacheva)
         str += tagName(trigerPokholkov)
@@ -44,7 +43,8 @@ def get_text_message(message):
         str += tagName(trigerLamskov)
         str += tagName(trigerMikova)
         str += tagName(trigerGudkov)
-        str += tagName(trigerSedov)
+        if 'не удалось найти информацию о финансировании' not in targetText:
+            str += tagName(trigerSedov)
     if "/lots2020/" in targetText:
         str += tagName(trigerGudkov)
         str += tagName(trigerSedov)
@@ -54,11 +54,12 @@ def get_text_message(message):
         str +=tagName(trigerMikova)
         str +=tagName(trigerLamskov)
         str +=tagName(trigerGudkov)
-    try:
-        print(str)
-    except:
-        print("Не найдено слово-триггер")
+    print(str)
     if ("@" in str):
         bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.id, text= str)
-
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(none_stop=True,interval=0)
+    except Exception as _ex:
+        print(_ex)
+        sleep(5)
